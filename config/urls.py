dcd989700c4cf6ff91a02035ed2eb537308981f4
adrianapalushi <adrianapalushi@gmail.com>
2026-04-10 11:32:21 +0200

    Add user registration endpoint with Django REST Framework and JWT setup

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # JWT endpoints
+    path('api/auth/register/', include('users.urls')),
+
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
