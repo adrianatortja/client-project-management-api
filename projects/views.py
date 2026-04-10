cde36ae3cdbbc8bb85aa29c059c38a84ea1117e1
adrianapalushi <adrianapalushi@gmail.com>
2026-04-10 16:16:43 +0200

    Add authenticated project creation API

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProjectListCreateView(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
