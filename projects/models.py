5f22c1b88b6158eb31274e7381a8edd68d906d2a
adrianapalushi <adrianapalushi@gmail.com>
2026-04-11 12:34:17 +0200

    Add Task model with Project relationship

--- projects/models.py
+++ projects/models.py
@@ -19,3 +19,15 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Task(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
