6c6849c3b4572cd0ea9d714907eff6e73bd49601
adrianapalushi <adrianapalushi@gmail.com>
2026-04-11 17:03:01 +0200

    Add task ownership validation to serializer

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from .models import Project, Task
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ['id', 'title', 'description', 'status', 'created_at']
-        
+
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'project', 'title']
+
+    def validate_project(self, value):
+        request = self.context.get('request')
+
+        if value.user != request.user:
+            raise serializers.ValidationError("You can only add tasks to your own projects.")
+
+        return value
+    
         
         
