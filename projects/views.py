e23c9e8999c785e85ecb258d5005286f1819282e
adrianapalushi <adrianapalushi@gmail.com>
2026-04-14 14:45:46 +0200

    Add filtering, search, and ordering to API

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
 
@@ -6,12 +7,18 @@ from .serializers import ProjectSerializer, TaskSerializer
 class ProjectListCreateView(generics.ListCreateAPIView):
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status']
+    search_fields = ['title']
+    ordering_fields = ['created_at', 'title']
+    ordering = ['-created_at']
 
     def get_queryset(self):
         return Project.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
 
 
 class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -25,9 +32,13 @@ class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
 class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['completed']
+    ordering_fields = ['created_at', 'title']
+    ordering = ['-created_at', '-id']
 
     def get_queryset(self):
-        return Task.objects.filter(project__user=self.request.user).order_by('-created_at', '-id')
+        return Task.objects.filter(project__user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save()
